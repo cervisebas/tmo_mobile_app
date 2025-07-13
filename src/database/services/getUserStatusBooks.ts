167116b4bc8a6a27251c0f6559404147a735c697
“Sebástian <cervisebas@gmail.com>
2025-07-13 17:11:56 -0300

    feat: implement list of save books

---
+++ src/database/services/getUserStatusBooks.ts
@@ -0,0 +1,41 @@
+import { UserBookStatus } from "~/api/enums/UserBookStatus";
+import { db } from "../database";
+import { BookInfoModel } from "../schemas/BookInfoModel";
+import { BookUserStatusByBookInfoModel } from "../schemas/BookUserStatusByBookInfoModel";
+import { and, eq } from "drizzle-orm";
+import { DatabaseTableName } from "../enums/DatabaseTableName";
+
+export async function getUserStatusBooks(status: UserBookStatus) {
+  const books = await db
+    .select()
+    .from(BookUserStatusByBookInfoModel)
+    .innerJoin(
+      BookInfoModel,
+      eq(
+        BookInfoModel.id,
+        BookUserStatusByBookInfoModel.id_bookinfo,
+      ),
+    )
+    .where(
+      and(
+        eq(
+          BookUserStatusByBookInfoModel.status,
+          status,
+        ),
+        eq(
+          BookUserStatusByBookInfoModel.marked,
+          true,
+        ),
+      ),
+    );
+
+  return books.map(value => ({
+    id: value[DatabaseTableName.BOOKS_INFO].id,
+    path: value[DatabaseTableName.BOOKS_INFO].path,
+    url: value[DatabaseTableName.BOOKS_INFO].url,
+    title: value[DatabaseTableName.BOOKS_INFO].title,
+    picture: value[DatabaseTableName.BOOKS_INFO].picture,
+    stars: value[DatabaseTableName.BOOKS_INFO].stars,
+    type: value[DatabaseTableName.BOOKS_INFO].type,
+  }));
+}
--- src/screens/MyProfileScreen/MyProfile.tsx
+++ src/screens/MyProfileScreen/MyProfile.tsx
@@ -2,8 +2,18 @@ import { Appbar } from "react-native-paper";
 import { AppbarHeader } from "~/common/components/AppbarHeader";
 import PrincipalView from "~/common/components/PrincipalView";
 import { DrawerScreenProps } from "~/common/interfaces/DrawerScreenProps";
+import { BookStatusPanel } from "../BookInfoScreen/components/BookStatusPanel";
+import { useProfileBooks } from "./hooks/useProfileBooks";
+import { useState } from "react";
+import { UserBookStatus } from "~/api/enums/UserBookStatus";
+import SafeArea from "~/common/components/SafeArea";
+import ListOfBooks from "~/common/components/ListOfBooks";
+import { View } from "react-native";
 
 export function MyProfileScreen(props: DrawerScreenProps) {
+  const [select, setSelect] = useState(UserBookStatus.WATCH);
+  const {stateList, books} = useProfileBooks(select);
+
   return (
     <PrincipalView hideKeyboard>
       <AppbarHeader mode={'small'}>
@@ -15,6 +25,27 @@ export function MyProfileScreen(props: DrawerScreenProps) {
           title={'Mi Perfil'}
         />
       </AppbarHeader>
+
+      <SafeArea.View
+        className={'flex-1'}
+        expandDisableTop
+        expandDisableBottom
+      >
+        <View className={'w-full mx-[12]'}>
+          <BookStatusPanel
+            {...stateList}
+            onToggleStatus={setSelect}
+          />
+        </View>
+
+        <View className={'flex-1'}>
+          <ListOfBooks
+            data={books}
+            keyExtractor={'my-profile-item-{id}'}
+          />
+        </View>
+
+      </SafeArea.View>
     </PrincipalView>
   );
 }
