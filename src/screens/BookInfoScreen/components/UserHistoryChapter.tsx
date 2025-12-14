5d9d8dc03569ba946cfbe224ec867a82cd25dfec
Sebastian Cerviño <cervisebas@gmail.com>
2025-12-14 14:21:20 -0300

    feat: se añadio una opcion de siguiente capitulo al apartado de continuacion de lectura

--- src/screens/BookInfoScreen/components/UserHistoryChapter.tsx
+++ src/screens/BookInfoScreen/components/UserHistoryChapter.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import { View } from 'react-native';
 import { Divider, Text } from 'react-native-paper';
 import ItemWithIcon from '~/common/components/ItemWithIcon';
@@ -6,6 +6,17 @@ import { useUserChapterBookHistory } from '~/database/hooks/useUserChapterBookHi
 import { onPressChapterItem } from '../scripts/onPressChapterItem';
 import { ThemeContext } from '~/common/providers/ThemeProvider';
 import { ChapterInterface } from '~/api/interfaces/ChapterInterface';
+import Carousel from 'react-native-reanimated-carousel';
+import {
+  ItemListChapter,
+  useHistoryChapterList,
+} from '../hooks/useHistoryChapterList';
+import { useSharedValue } from 'react-native-reanimated';
+import {
+  CarouselRenderItemInfo,
+  ICarouselInstance,
+} from 'react-native-reanimated-carousel/lib/typescript/types';
+import useDimension from '~/common/hooks/useDimension';
 
 interface IProps {
   id_bookinfo: number;
@@ -13,31 +24,74 @@ interface IProps {
   chapters: ChapterInterface[];
 }
 
+const ITEM_HEIGHT = 69;
+
 export const UserHistoryChapter = React.memo(function (props: IProps) {
   const { theme } = useContext(ThemeContext);
-  const { chapter, option } = useUserChapterBookHistory(props.id_bookinfo);
-
-  const onPressItem = useCallback(() => {
-    if (!chapter) {
-      return;
-    }
-
-    onPressChapterItem({
-      chapter: chapter,
-      primaryColor: theme.colors.primary,
-      book_url: props.book_url,
-      id_bookinfo: props.id_bookinfo,
-      chapters: props.chapters,
-    });
-  }, [
-    chapter,
-    props.book_url,
+  const { chapter: currentChapter, option: currentOption } =
+    useUserChapterBookHistory(props.id_bookinfo);
+  const { itemListChapter } = useHistoryChapterList(
+    currentChapter,
     props.chapters,
-    props.id_bookinfo,
-    theme.colors.primary,
-  ]);
+    currentOption,
+  );
+  const [WINDOW_WIDTH] = useDimension('window');
 
-  if (!chapter || !option) {
+  const WIDTH_ELEMENTS = WINDOW_WIDTH - 16;
+
+  const refCarousel = useRef<ICarouselInstance>(null);
+
+  // Animation Values
+  const scrollOffsetValue = useSharedValue<number>(0);
+
+  // Actions
+  const onPressItem = useCallback(
+    (chapter?: ChapterInterface) => {
+      if (!chapter) {
+        return;
+      }
+
+      onPressChapterItem({
+        chapter: chapter,
+        primaryColor: theme.colors.primary,
+        book_url: props.book_url,
+        id_bookinfo: props.id_bookinfo,
+        chapters: props.chapters,
+      });
+    },
+    [
+      currentChapter,
+      props.book_url,
+      props.chapters,
+      props.id_bookinfo,
+      theme.colors.primary,
+    ],
+  );
+
+  // Render Function
+  const renderItem = useCallback(
+    ({ item: [chapter, option] }: CarouselRenderItemInfo<ItemListChapter>) => (
+      <ItemWithIcon
+        leftIcon={'play-pause'}
+        title={chapter?.title || ''}
+        description={option?.title}
+        style={{ width: WIDTH_ELEMENTS }}
+        onPress={() => onPressItem(chapter)}
+        fixHeight={ITEM_HEIGHT}
+      />
+    ),
+    [currentOption],
+  );
+
+  useEffect(() => {
+    refCarousel.current?.scrollTo({
+      count: 0,
+      animated: false,
+    });
+    scrollOffsetValue.value = -0;
+  }, [currentChapter]);
+
+  if (!currentChapter || !currentOption) {
     return <></>;
   }
 
@@ -46,12 +100,27 @@ export const UserHistoryChapter = React.memo(function (props: IProps) {
       <View className={'flex-col gap-[8]'}>
         <Text variant={'titleLarge'}>Continuar</Text>
 
-        <ItemWithIcon
-          leftIcon={'play-pause'}
-          title={chapter.title}
-          description={option.title}
-          onPress={onPressItem}
-        />
+        <View className={'w-full'}>
+          <Carousel
+            ref={refCarousel}
+            loop={false}
+            snapEnabled={true}
+            pagingEnabled={true}
+            autoPlayInterval={2000}
+            width={WIDTH_ELEMENTS}
+            height={ITEM_HEIGHT}
+            style={{
+              width: WIDTH_ELEMENTS,
+              height: ITEM_HEIGHT,
+            }}
+            data={itemListChapter}
+            defaultScrollOffsetValue={scrollOffsetValue}
+            onConfigurePanGesture={(gestureChain) =>
+              gestureChain.activeOffsetX([-10, 10])
+            }
+            renderItem={renderItem}
+          />
+        </View>
       </View>
 
       <Divider />
